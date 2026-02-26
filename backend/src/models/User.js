8bb02453435bd0b6db04b208c61a69da375402a8
Sunay Kulkarni <sunaykulkarni17@gmail.com>
2026-02-26 21:46:32 +0530

    fixed endpoint issue

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -31,12 +31,12 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+// Hash password before saving - no next() needed in Mongoose 5+
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
